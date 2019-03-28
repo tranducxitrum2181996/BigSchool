45b79d48c8700334122380914f27b921acbc0e77
Administrator <Administrator@PC026>
2019-03-28 11:16:17 +0700

    fdgfdg

---
+++ BigShool/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigShool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public ApplicationUser LecturerId { get; set; }
+        [Required]
+        [stringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+
+    }
+   
+}
---
+++ BigShool/Models/stringLengthAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BigShool.Models
+{
+    internal class stringLengthAttribute : Attribute
+    {
+        private int v;
+
+        public stringLengthAttribute(int v)
+        {
+            this.v = v;
+        }
+    }
+}
